ed37399d1dd5fa8d3cd886432d357911a1c09b8f
GuillaumeMalecha <guillaume.malecha@gmail.com>
2023-03-08 17:15:04 +0100

    adresses json db ok

---
+++ src/Controller/AdressesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CodePostal;
+use App\Entity\Commune;
+use App\Entity\Localite;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdressesController extends AbstractController
+{
+    /**
+     * @Route("/adresses", name="app_adresses")
+     */
+    public function index(): Response
+    {
+        return $this->render('adresses/index.html.twig', [
+            'controller_name' => 'AdressesController',
+        ]);
+    }
+
+    /**
+     * @Route("/ajoutadresses", name="ajoutadresses")
+     */
+    public function ajoutAdresses(EntityManagerInterface $entityManager){
+
+        $json = file_get_contents('../public/data/Region-Ville-CodePostal.json');
+        $data = json_decode($json, true);
+
+        foreach ($data as $item) {
+            $commune = new Commune();
+            $localite = new Localite();
+            $cp = new CodePostal();
+            $commune->setCommune($item['region']);
+            $localite->setLocalite($item['ville']);
+            $cp->setCodePostal($item['codePostal']);
+            $entityManager->persist($commune);
+            $entityManager->persist($localite);
+            $entityManager->persist($cp);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_adresses');
+    }
+}
+
